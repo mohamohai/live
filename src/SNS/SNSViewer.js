a3e53ad41a162a679b4b7e719fbe03e1e1113cb0
jh <ahwjs1022@gmail.com>
2023-02-28 16:19:19 +0900

    styled components change

--- src/SNS/SNSViewer.js
+++ src/SNS/SNSViewer.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { collection, getDocs } from "firebase/firestore";
 import firebaseConfig from '../firebase.js';
-import { NickName,Account,ContentText,CircleText, UserPic } from "./SNSViweerZip.js"; 
+import {SNSViewerOne,FlexOnlyDiv,SNSViewerOneUserContent, NickName,Account,ContentText,CircleText, UserPic } from "./SNSViweerZip.js"; 
 
 import './SNSViewer.css'
 function SNSViewer(){
@@ -32,16 +32,19 @@ function searchlog(){
     return(<div className="SNSViewer">
             {SearchAccountHit? <div>{SearchAccount.map((row,key)=>{
               return(
-                <div key={key} className="SNSViewerOne">
-                  <div className="SNSViewerOneUserImg">
+                <SNSViewerOne key={key}>
+                  <FlexOnlyDiv>
                     <UserPic url={`https://jonghyunportfolio.s3.ap-northeast-2.amazonaws.com/%EC%A7%B1.jpg`}></UserPic>
-                  </div>
-                  <div className="SNSViewerOneUserContent">
-                    <div><NickName>{row.name}</NickName>  <Account>{row.id}@naver.com</Account><span>time</span></div>
+                  </FlexOnlyDiv>
+                  <SNSViewerOneUserContent>
+                    <FlexOnlyDiv>
+                      <NickName>{row.name}</NickName>
+                      <Account>{row.id}@naver.com</Account> 
+                    </FlexOnlyDiv>
                     <ContentText>xcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontentcontent</ContentText>
-                    <div className="SNSViewerOneUserContentImg"><img src="https://jonghyunportfolio.s3.ap-northeast-2.amazonaws.com/%EC%A7%B1.jpg"></img></div>
-                  </div>
-          </div>
+                    <div className="SNSViewerOneUserContentImg"><img src="https://jonghyunportfolio.s3.ap-northeast-2.amazonaws.com/%EB%B0%B0.jpg"></img></div>
+                  </SNSViewerOneUserContent>
+                </SNSViewerOne>
 
             )
           })}</div> :<p>여기 스켈레톤 ui 넣을 예정</p>} 
--- src/SNS/SNSViweerZip.js
+++ src/SNS/SNSViweerZip.js
@@ -1,30 +1,46 @@
 import styledComponents from 'styled-components';
 
+const SNSViewerOne = styledComponents.div`
+width: 60vw;
+    min-width: 500px;
+    padding: 20px 70px 20px 20px;
+    height: auto;
+    border: solid 1px rgb(0,0,0,0.2);
+    border-radius: 15px;
+    display: flex;
+    flex: row;
+    justify-content: center;
+    margin: 10px 0;
+`;
+const FlexOnlyDiv = styledComponents.div`
+`;
+
+
 const NickName = styledComponents.span`
   font-size: 1.1em;
   text-align: center;
   font-weight:bold;
   cursor: pointer;
   &:hover{
-    font-size: 1.15em;
     transition: all 0.3s;
-    color:red;
+    text-decoration: underline;
+    
   }
 `;
 
 const Account = styledComponents.span`
-    font-size: 0.8em;
-    text-align: right;
-    color:rgb(150,150,150);
-    cursor: pointer;
+  font-size: 0.8em;
+  text-align: right;
+  color:rgb(150,150,150);
+  cursor: pointer;
 `;
 
 const ContentText = styledComponents.div`
-    font-size: 1.0em;
-    text-align: left;
-    width: 100%;
-    word-break: break-all;
-    cursor: pointer;
+  font-size: 1.0em;
+  text-align: left;
+  width: 100%;
+  word-break: break-all;
+  cursor: pointer;
 `;
 const UserPic = styledComponents.div`
   width: 50px;
@@ -33,12 +49,20 @@ const UserPic = styledComponents.div`
   background: url(${props=>props.url}) center center / cover;
 `;
 const CircleText = styledComponents.div`
-width: 5rem;
-height: 5rem;
-background: ${props => props.color || 'black'};
-border-radius: 50%;
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color || 'black'};
+  border-radius: 50%;
 `;
-export { NickName, Account, ContentText, CircleText,UserPic };
+
+const SNSViewerOneUserContent = styledComponents.div`
+  display: flex;
+  padding: 0px 10px;
+  flex-direction: column;
+  width: 90%;
+  text-align: left;
+`;
+export {SNSViewerOne, FlexOnlyDiv,SNSViewerOneUserContent, NickName, Account, ContentText, CircleText,UserPic };
 
 
 
