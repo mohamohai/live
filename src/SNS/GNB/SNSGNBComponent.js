8264281571eeb82ed4bd2fc537ae3a485d94f955
jh <ahwjs1022@gmail.com>
2023-03-02 13:42:14 +0900

    checkpoint

--- src/SNS/GNB/SNSGNBComponent.js
+++ src/SNS/GNB/SNSGNBComponent.js
@@ -1,24 +1,81 @@
-import { useEffect } from "react";
+import { useEffectm, useState } from "react";
 import styledComponents from 'styled-components';
 
-import {HiUser, HiUserAdd, } from 'react-icons/hi'
 import {AiOutlinePlus,AiOutlineSearch, AiOutlineBell, AiOutlineHome} from 'react-icons/ai';
 import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 
 function SNSGNBComponent(props){
     const GNBUrl = props.GNBUrl;
     const IconName = props.IconName;
     const GNBText = props.GNBText;
    
+
+
+    const [WriteUser,       setWriteUser   ]=useState('');
+    const [WriteUserPic ,   setWriteUserPic]=useState('');
+    const [WriteTitle,      setWriteTitle  ]=useState('');
+    const [WriteContent,    setWriteContent]=useState('');
+    const [WriteTime,       setWriteTime   ]=useState('');
+    const [WritePicArr,     setWritePicArr ]=useState([]);
+    
+
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+
+
     return(
         <GNBRow>
             <Link to={`./${GNBUrl}`}>
             {IconName==="AiOutlineHome"? <AiOutlineHome className="GNBIcon" size='1.5rem'  fill="black"/>: 
                 IconName==="AiOutlineSearch" ? <AiOutlineSearch className="GNBIcon" size='1.5rem'  fill="black"/> : 
-                    IconName==="AiOutlinePlus" ? <AiOutlinePlus className="GNBIcon" size='1.5rem'  fill="black"/> :
+                    IconName==="AiOutlinePlus" ? <AiOutlinePlus onClick={handleShow} className="GNBIcon" size='1.5rem'  fill="black"/> :
                         IconName==="AiOutlineBell" ? <AiOutlineSearch className="GNBIcon" size='1.5rem'  fill="black"/> : "컴포넌트 함수로 호출 찾아보고"}
              <GNBRowSpan>{GNBText}</GNBRowSpan>
              </Link>
+             <AiOutlinePlus onClick={handleShow} className="GNBIcon" size='1.5rem'  fill="black"/>
+
+
+             <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>User Id</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+                onChange={(e)=>setWriteUser(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
         </GNBRow>
     )
 }
